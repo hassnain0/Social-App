9830dd43de6eae1de0c4eaec20706397cdd59b94
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-08-29 23:28:00 +0500

    RichTextEditor

--- components/RichTextEditor.jsx
+++ components/RichTextEditor.jsx
@@ -1,10 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { actions, RichToolbar } from "react-native-pell-rich-editor";
+import { theme } from "../constants/theme";
 
-const RichTextEditor = ({ props }) => {
+const RichTextEditor = ({ editorRef, onChange }) => {
   return (
-    <View style={{ minHeight: 205 }}>
+    <View style={{ minHeight: 285 }}>
       <RichToolbar
         actions={[
           actions.insertImage,
@@ -21,19 +22,42 @@ const RichTextEditor = ({ props }) => {
           actions.checkboxList,
           actions.undo,
           actions.redo,
+          actions.code,
+          actions.line,
+          actions.heading1,
+          actions.heading4,
         ]}
         style={styles.richBar}
         flatContainerStyle={styles.listStyle}
-        editor={props.editorRef}
         disabled={false}
+        editorRef={editorRef}
+        iconMap={{
+          [actions.heading1]: ({ tintColor }) => (
+            <Text style={{ color: tintColor }}>H1</Text>
+          ),
+          [actions.heading4]: ({ tintColor }) => (
+            <Text style={{ color: tintColor }}>H4</Text>
+          ),
+        }}
+        selectedIconTint={theme.colors.primaryDark}
       />
-      <Text>RichTextEditor</Text>
+      <RichTextEditor
+        ref={editorRef}
+        containerStyle={styles.rich}
+        placeholder={"What's on your mind"}
+        onChange={onChange}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  richBar: {},
+  richBar: {
+    borderTopRightRadius: theme.radius.xl,
+    borderTopLeftRadius: theme.radius.xl,
+    backgroundColor: theme.colors.gray,
+  },
   listStyle: {},
+  rich: {},
 });
 export default RichTextEditor;
